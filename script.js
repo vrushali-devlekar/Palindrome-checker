c7c694d79816b2d0266b7e7828172ffa2d946e15
Vrushali <118001762+vrushali-devlekar@users.noreply.github.com>
2025-08-25 19:19:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let input = document.getElementById("input");
+
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+function check() {
+  let value = input.value;
+  let reverse = reverseString(value);
+  if (value == reverse) {
+    alert("P A L I N D R O M E");
+  } else {
+    alert("Not today");
+  }
+}
